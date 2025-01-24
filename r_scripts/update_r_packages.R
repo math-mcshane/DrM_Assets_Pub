7758f67976a5cef49516200afb7b353a47e61937
Dr. Ryan McShane <48807452+math-mcshane@users.noreply.github.com>
2025-01-24 16:05:49 -0600

    Create update_r_packages.R
    After testing on my own machine and checking with students to see what works on their machines, I am putting this file here to direct students towards if they want to install R packages without hassle.

---
+++ r_scripts/update_r_packages.R
@@ -0,0 +1,67 @@
+############################################
+## Script written by Dr. Ryan McShane     ##
+## www.github.com/math-mcshane            ##
+## www.ryanmcshane.com                    ##
+############################################
+
+## This script will update every R package 
+##  you have already installed to its 
+## newest version.
+## It will also check required dependencies 
+##  and install those if they are not 
+##  already installed.
+
+if(!any(installed.packages()[,1] == "pak")) install.packages("pak")
+pak::pak("tidyverse")
+no_vec_coalesce = function(data) {
+  pak_table = {{data}}
+  columns_available = pak::pkg_list() |> 
+    tibble::as_tibble() |>
+    within(data = _, list())
+  make_own = lapply(
+      X = c("remotetype", "remoteusername", "remoterepo"), 
+      FUN = exists, 
+      where = columns_available
+    ) |> 
+    do.call(what = c, args = _) |>
+    all()
+  
+  if (exists("remotepkgref", where = columns_available)) {
+    cat("Using remotepkgref.\n")
+    package_vector = ifelse(
+      test = pak_table[["repository"]] != "CRAN" | pak_table[["repository"]] |> is.na(), 
+      yes = pak_table[["remotepkgref"]], 
+      no = pak_table[["package"]]
+    )
+  } else if (make_own) {
+    cat("Using wrangled remotepkgref.\n")
+    pak_table = pak_table |>
+      dplyr::mutate(remotepkgref = paste0(
+        remotetype, 
+        "::", 
+        remoteusername, 
+        "/", 
+        remoterepo)
+      )
+    package_vector = ifelse(
+        test = pak_table[["repository"]] != "CRAN" | pak_table[["repository"]] |> is.na(), 
+        yes = pak_table[["remotepkgref"]], 
+        no = pak_table[["package"]]
+      ) |> 
+      purrr::discard(is.na) 
+  } else {
+    cat("Using vanilla package list.\n")
+    package_vector = pak_table[["package"]] |> purrr::discard(is.na) 
+  }
+return(package_vector)
+}
+
+update_paks = function() {
+  package_vector = pak::pkg_list() |> 
+    tibble::as_tibble() |> 
+    no_vec_coalesce()
+  pak::meta_update()
+  pak::pkg_install(package_vector, upgrade = TRUE, ask = FALSE)
+  pak::cache_clean()
+}
+update_paks()
